7b6e8957f89fbfff45db23412b10eba7f7250131
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2025-02-04 13:51:25 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "prefix" {
+  default = "andre"
   description = "This prefix will be included in the name of most resources."
 }
 
@@ -45,6 +46,6 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
+  default     = "placedog.net"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
